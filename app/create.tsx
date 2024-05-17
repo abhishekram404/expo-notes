7b922881277d04538c011daa36e1bf7a8a1267f2
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-05-17 17:28:45 +0545

    add header in create page

--- app/create.tsx
+++ app/create.tsx
@@ -1,14 +1,15 @@
 import Pill from "@/components/Pill";
 import PillsGroup from "@/components/PillsGroup";
 import { Colors } from "@/constants/Colors";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 import {
-    ScrollView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TextInputProps,
-    View,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
 } from "react-native";
 
 const inputConfigs: TextInputProps = {
@@ -20,6 +21,26 @@ const inputConfigs: TextInputProps = {
 const create = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={[styles.headerButton]}>
+          <Ionicons
+            style={styles.headerButtonIcon}
+            name="chevron-back"
+            size={20}
+            color={Colors.dark.icon}
+          />
+        </View>
+        <Text style={styles.headerTitle}>Create Note</Text>
+
+        <View style={[styles.headerButton]}>
+          <MaterialCommunityIcons
+            style={styles.headerButtonIcon}
+            name="check"
+            size={20}
+            color={Colors.dark.primary}
+          />
+        </View>
+      </View>
       <TextInput
         placeholder="Title"
         style={styles.titleInput}
@@ -53,7 +74,24 @@ const styles = StyleSheet.create({
     padding: 10,
     gap: 15,
   },
-  header: {},
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.dark.text,
+  },
+  headerButton: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: Colors.dark.secondary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerButtonIcon: {},
   title: {
     fontSize: 30,
     fontWeight: "bold",
